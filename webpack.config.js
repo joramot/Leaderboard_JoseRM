aba9d62d947e8d9db293e632685c5c79901d3600
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-05-16 14:03:05 -0600

    Update index.html and add style.css

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,51 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
   },
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-    template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].main.js',
     path: path.resolve(__dirname, 'dist'),
-   clean: true,
+    clean: true,
   },
+
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+            },
+          },
+        ],
+      },
     ],
   },
+
   optimization: {
     runtimeChunk: 'single',
   },
